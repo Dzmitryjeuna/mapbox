a1a1f02713520d753c58c03894090002eff39b56
Diana Shafarenko <shafarenko.di@gmail.com>
2024-04-12 21:14:35 +0300

    upgrade

--- telegram.php
+++ telegram.php
@@ -1,29 +1,28 @@
+
+
 <?php
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
-$company = $_POST['company'];
-$task = $_POST['task'];
-$token = "6778989090:AAHSuDdef3mBfQ3Oz2I3LSk2pPpXepZ-wmw";
-$chat_id = "-1001990264511";
+$token = "";
+$chat_id = "";
 $arr = array(
-  'Имя: ' => $name,
+  'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
-  'Email:' => $email,
-  'Сфера:' => $company,
-  'Требуется:' => $task
+  'Email' => $email
 );
 
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$response = file_get_contents("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}");
+$response = json_decode($response, true);
 
-if ($sendToTelegram) {
-  header('Happpy');
+if ($response && $response['ok']) {
+    // header('Location: thank-you.html');
 } else {
-  echo "Error";
+    echo "Error";
 }
 ?>
